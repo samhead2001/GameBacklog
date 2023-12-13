c203d48f73fe51049300d2416b5f8d2ca6f80a54
srhusted@iastate.edu <srhusted@iastate.edu>
2023-12-13 17:13:28 -0600

    Added code for viewing purposes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home';
+import { initializeApp } from "firebase/app";
+import { app } from "./config/config";
+import { getAnalytics } from "firebase/analytics";
+import SignUpForm from './pages/SignUpForm';
+import DisplayGamePage from './pages/DisplayGame';
+import DisplayGameSearch from './pages/DisplayGameSearch';
+import DisplayGameList from './pages/DisplayGameList';
+import DisplayGameByImagePage from './pages/DisplayGameByImage';
+
+
+// Initialize Firebase
+// const app = initializeApp(config.firebaseConfig);
+const analytics = getAnalytics(app);
+
+export interface IApplicationProps {}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route 
+        path="/" 
+        element={
+            <HomePage />
+        } 
+        />
+        <Route path="/login" element={<SignUpForm />} />
+        {/* <Route path="/game-detail-random" element={<RandomDisplayGamePage />} /> */}
+        <Route path="/game-search" element={<DisplayGameSearch />} />
+        <Route path="/game-search-image" element={<DisplayGameByImagePage />} />
+        <Route path="/game-detail" element={<DisplayGamePage />} />
+        <Route path="/backlog" element={<DisplayGameList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
